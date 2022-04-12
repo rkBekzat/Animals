1ca20d6566d4abd7a729a6893bd996fb43f65ca4
Bekzat <rakhimbayevbekzat@gmail.com>
2022-04-12 19:37:20 +0500

    register model to admin, and create page where you can add animal(not finish)

--- kingdom/main/views.py
+++ kingdom/main/views.py
@@ -1,4 +1,17 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .models import  Animal
 
 def index(requst):
-    return render(requst, 'main/index.html')
+    animal = Animal.objects.all()
+    return render(requst, 'main/index.html', {'animal':animal})
+
+def create(request):
+    if request.method == "POST":
+        animal = Animal()
+        animal.title = Animal.POST.get("title")
+        animal.bio = Animal.POST.get("bio")
+        animal.firstname = Animal.POST.get("firstname")
+        animal.lastname = Animal.POST.get("lastname")
+        animal.save()
+    return render(request, 'main/create.html')
