c4ec3a99c79f4b79b6468e68744225ba0765dd4e
Bekzat <rakhimbayevbekzat@gmail.com>
2022-04-12 17:30:29 +0500

    made model

--- kingdom/main/models.py
+++ kingdom/main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Animal(models.Model):
+    bio = models.TextField("Bio")
+    firstname = models.CharField("First name", max_length=30)
+    lastname = models.CharField("Last name", max_length=30)
+    title = models.CharField(max_length=30)
