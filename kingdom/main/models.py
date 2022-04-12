d8182c2586aefb9739c0c90f19173e5539585ea7
Bekzat <rakhimbayevbekzat@gmail.com>
2022-04-13 02:06:39 +0500

    editing front in index.html, delete.html, create.html, detail.html, and also deleting main.py which useless in our case

--- kingdom/main/models.py
+++ kingdom/main/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Animal(models.Model):
-    avatar = models.TextField("Link to the photo", default="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQSRpiKVmDu4KctY07T_FzEnYkdYFBCwoqU2NDlNc6HKej9jXHjZQ")
+    avatar = models.TextField("Link to the photo")
     bio = models.TextField("Bio")
     firstname = models.CharField("First name", max_length=30)
     lastname = models.CharField("Last name", max_length=30)
